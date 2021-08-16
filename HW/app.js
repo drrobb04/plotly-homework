519173027599407e7c0fb29d502015c195bc7f6c
drrobb04 <drrobb04@gmail.com>
2021-08-16 17:18:47 -0600

    file reorganization

---
+++ HW/app.js
@@ -0,0 +1,30 @@
+//make sample function
+
+
+
+
+
+
+
+
+
+
+// create charts function
+
+
+// create horizontal bar chart
+
+
+// create bubble chart
+
+
+
+
+
+
+// init function - event listener
+
+
+
+
+
--- StarterCode/static/js/app.js
+++
@@ -1,7 +0,0 @@
-//read in json data
-var data = d3.json("/data/samples.json", function(data) {
-    console.log(data);
-});
-
-
-
