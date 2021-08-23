2c239aa25778bf61f814f315cba2856a0aee2053
drrobb04 <drrobb04@gmail.com>
2021-08-23 10:39:35 -0600

    graph data set up

--- HW/app.js
+++ HW/app.js
@@ -1,13 +1,17 @@
 //make sample function
 function makeSampleData(sample) {
     d3.json("/data/samples.json").then(function(data) {
+        // data set up
         var sampleData = data.metadata;
         var sampleArray = sampleData.filter(sampleObj => sampleObj.id == sample);
-        var sample = sampleArray[0];
-
-
-
-    }) 
+        var sampleSet = sampleArray[0];
+        // dropdown set up
+        var dropDown = d3.select("#sample-metadata");
+        dropDown.html("");
+        Object.entries(sampleSet).forEach(([key, value]) => {
+            dropDown.append("h6").text(`${key.toUpperCase()}: ${value}`);
+          });
+    });
 }
 
 
@@ -17,6 +21,15 @@ function makeSampleData(sample) {
 // create charts function
 function makeCharts(sample) {
     d3.json("/data/samples.json").then(function(data) {
+        // data set up
+        var graphData = data.samples;
+        var graphArray = graphData.filter(sampleObj => sampleObj.id == sample);
+        var graphSet = graphArray[0];
+
+        //var set up
+        var sampleValues = graphSet.sample_values;
+        var otuIds = graphSet.otu_ids;
+        var otuLabels = graphSet.otu_labels;
 
     // data
     var 
